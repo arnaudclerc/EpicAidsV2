bd9ef1309f966367d483ef295b827518c68006ad
Arnaud Clerc <arnaud.clerc@epitech.eu>
2018-12-20 00:02:07 +0100

    I fucked up it's OOhOO am

--- currentSketch/currentSketch.ino
+++ currentSketch/currentSketch.ino
@@ -1,53 +1,119 @@
+
 #include <Wire.h>
 #include <SPI.h>
-#include <Adafruit_PN532.h> // OLD   #include <PN532.h>
+#include <Adafruit_PN532.h>
+#include <EEPROM.h>
+#include <DS3231.h>
 
-#define NSS   (10)
+#define IRQ   (2)
+#define RESET (3)
+
+Adafruit_PN532 nfc(IRQ, RESET);
+DS3231  rtc(SDA, SCL);
+
+boolean add = false;
+uint8_t ite = 0;
+uint8_t admin[] = { 1, 114, 25, 170 };
+Time  t;
 
 int out1 = 9;
 int out2 = 6;
 float V1 = 0;
 float V2 = 0;
 
-Adafruit_PN532 nfc(NSS);
-
-boolean add = false;
-uint8_t ite = 0;
-uint8_t admin[] = { 1, 114, 25, 170 };
-uint8_t list[][7] = {
-                { 4, 62, 97, 50, 198, 78, 128 },
-                { 4, 95, 168, 50, 198, 78, 128 }
-        };
-
+struct ms
+{
+    uint8_t  content[7];
+};
 
 int cc(uint8_t a[], uint8_t b[], uint8_t l)
 {
-        for(int i = 0; i < l; i++)
+    for(int i = 0; i < l; i++)
+    {
+        if (a[i] != b[i])
+            return (1);
+    }
+    return (0); 
+}
+
+void removeEEPROM(uint8_t i)
+{
+    uint8_t  tmp[7];
+    uint8_t  stk;
+    
+    for (int j = 0; j < 7; j++)
+        EEPROM.write(i * 7 + j, 0);
+    while (++i < ite)
+    {
+        for (int j = 0; j < 7; j++)
         {
-                if (a[i] != b[i])
-                        return (1);
+            stk = EEPROM.read(i * 7 + j);
+            EEPROM.write(i * 7 + j, 0);
+            EEPROM.write((i - 1) * 6 + j, stk);
         }
-        return (0); 
+    }
+    --ite;
+}
+
+struct ms  getEEPROM(uint8_t i)
+{
+    struct ms    lol;
+
+    for (int j = 0; j < 7; j++)
+          lol.content[j] = EEPROM.read(i * 7 + j);
+    return (lol);
+}
+
+void saveEEPROM(uint8_t val[])
+{
+    for (int i = 0; i < 7; i++)
+        EEPROM.write(7 * ite + i, val[i]);
+    ++ite;
+}
+
+
+void chargeEEPROM()
+{
+  uint8_t list[][7] = {
+    { 4, 62, 97, 50, 198, 78, 128 },
+    { 4, 95, 168, 50, 198, 78, 128 }
+  };
+  
+  uint8_t valid = EEPROM.read(0);
+  
+  if (valid == 0)
+  {
+      for (int i = 0; i < 256; i++)
+          EEPROM.write(i, 0);
+      saveEEPROM(list[0]);
+      saveEEPROM(list[1]);
+  }
+  else
+  {
+      int i = 0;
+      while (EEPROM.read(i++))
+          if (i % 6 == 0 && i != 0)
+              ++ite;
+  }
 }
 
 
 void setup(void)
 {
-        Serial.begin(115200);
-        pinMode(out1, OUTPUT);
-        pinMode(out2, OUTPUT);
-        digitalWrite(out1, HIGH);
-        digitalWrite(out2, HIGH);
-        delay(50);
-        nfc.begin();
-        uint32_t versiondata = nfc.getFirmwareVersion();
-        if (! versiondata) {
-                Serial.println("ERROR");
-                while(1);
-        }
-        nfc.SAMConfig();
+  Serial.begin(115200);
+  delay(50);
+  nfc.begin();
+  rtc.begin();
+  pinMode(12, INPUT_PULLUP);
+  uint32_t versiondata = nfc.getFirmwareVersion();
+  if (!versiondata)
+      while (1);
+  nfc.setPassiveActivationRetries(0x01);
+  nfc.SAMConfig();
+  attachInterrupt(0, onInterrupt, RISING);
+  chargeEEPROM();
 }
-        
+
 void    call_one(int pin)
 {
                 digitalWrite(pin, LOW);
@@ -55,67 +121,108 @@ void    call_one(int pin)
                 digitalWrite(pin, HIGH);       
 }
 
-void    loop(void)
-{    
-        uint8_t success;
-        uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 };
-        uint8_t uidLength;
-  
-        //  V1 = analogRead(A5) * (5 / 1023.0);
-        V2 = analogRead(A0) * (5 / 1023.0);
-          
-        if (V1 >= 0.3) 
-                call_one(out1);
-          
-        if (V2 >= 0.3) 
-                call_one(out2);
-          
-        delay(2);
-          
-        success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength);
-               
-        if (success && uidLength == 4 && cc(uid, admin, uidLength) == 0)
-        {
-                Serial.println("ADM");
-                add = !add;
-                delay(800);
-        }
-    
-        if (success && uidLength == 7)
-        {
-                if (add == 0)
-                {
-                        for (uint8_t i = 0 ; i < 36; i++)
-                        {
-                                if (cc(list[i], uid, uidLength) == 0)
-                                {
-                                        call_one(out2);
-                                        Serial.println("OKC");
-                                        delay(500);
-                                        break ; 
-                                }
-                        }
-                }
-                else if (add == 1)
-                {
-                        for (uint8_t i = 0; i < 36; i++)
-                        {
-                                if (cc(list[i], uid, uidLength) == 0)
-                                {
-                                        add = false;
-                                        Serial.println("OK-");
-                                        memcpy(list[i], list[ite-- + 1], uidLength);
-                                        delay(500); 
-                                        break ;
-                                }
-                        }
-                        if (add == 1)
-                        {
-                                Serial.println("OK+");
-                                add = false;
-                                delay(500);
-                                memcpy(list[ite++], uid, uidLength);
-                        }
-                }
-        }
+void onInterrupt()
+{
+    if (digitalRead(12) == LOW)
+    {
+        delay(1000);
+        for (int i = 0 ; i < 256 ; i++)
+            EEPROM.write(i, 0);
+    }
 }
+
+
+void loop(void)
+{  
+  uint8_t success;
+  uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 };
+  uint8_t uidLength;
+  struct ms      lol;
+  
+  success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength, 100);
+  
+    //V1 = analogRead(A5) * (5 / 1023.0);
+    V2 = analogRead(A0) * (5 / 1023.0);
+          
+    if (V1 >= 0.3) 
+        call_one(out1);
+          
+    if (V2 >= 0.3) 
+        call_one(out2);
+          
+    delay(2);
+
+    if (success && uidLength == 4 && cc(uid, admin, uidLength) == 0)
+    {
+        add = !add;
+        delay(800);
+    }
+    
+    if (success && uidLength == 7)
+    {
+        if (add == 0)
+        {
+            for (uint8_t i = 0 ; i <= 256; i+=2)
+            {
+                lol = getEEPROM(i);
+                if (cc(lol.content, uid, uidLength) == 0)
+                {
+                    call_one(out2);
+                    delay(500);
+                    break ; 
+                }
+            }
+        }
+        else if (add == 1)
+        {
+            for (uint8_t i = 0; i <= 256; i+=2)
+            {
+                lol = getEEPROM(i);
+                if (cc(lol.content, uid, uidLength) == 0)
+                {
+                    add = false;
+                    removeEEPROM(i);
+                    delay(500);
+                    break ;
+                }
+            }
+            if (add == 1)
+            {
+                t = rtc.getTime();
+                add = false;
+                delay(500);
+                saveEEPROM(uid);
+            }
+        }
+    }
+}
+
+
+
+
+/*
+  Some bullshit going on
+  
+  Working on it
+  t = rtc.getTime();
+  
+  // Send date over serial connection
+  Serial.print(":");
+  Serial.print(t.date, DEC);
+  Serial.print(". ");
+  Serial.print(rtc.getMonthStr());
+  Serial.print(" . ");
+  Serial.print(t.year, DEC);
+  Serial.print(" --- ");
+  
+  // Send Day-of-Week and time
+  Serial.print("");
+  Serial.print(t.dow, DEC);
+  Serial.print("th. ");
+  Serial.print(t.hour, DEC);
+  Serial.print("h");
+  Serial.print(t.min, DEC);
+  Serial.print("m");
+  Serial.print(t.sec, DEC);
+  Serial.println("s"); */
+
